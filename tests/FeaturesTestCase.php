ff5c678a2db17899d17d5518508d9e0ea36b583b
pcastro <pcastro@unrn.edu.ar>
2018-08-31 01:25:46 -0300

    Add feature test case

---
+++ tests/FeaturesTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Created by PhpStorm.
+ * User: castro
+ * Date: 31/08/2018
+ * Time: 01:19
+ */
+
+class FeaturesTestCase extends TestCase
+{
+
+    use DatabaseTransactions;
+}
