44b1b0e4dba97b9c69abf2180daa5886dda405c7
pcastro <pcastro@unrn.edu.ar>
2018-09-03 16:25:47 -0300

    Add list register

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,11 +6,19 @@ use App\Post;
 
 class PostController extends Controller
 {
+
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post, $slug)
     {
 //        abort_unless($post->slug == $slug, 404);
 
-        if($post->slug != $slug){
+        if ($post->slug != $slug) {
             return redirect($post->url, 301);
         }
 
